9f1af772536040f40ec08fb0587f91d6ac5d66a3
comels <come.l.s@hotmail.fr>
2022-02-04 14:57:56 +0000

    while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - all
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while ((a < 123) && (b < 90))
+	{	putchar(a);
+		a++;
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
