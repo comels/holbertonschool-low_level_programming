73d2e5b226e307bb0d4a9c59781ca6b27d9c59d2
comels <come.l.s@hotmail.fr>
2022-02-04 16:27:39 +0000

    012abc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - all
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b = 97;
+
+	while (a < 10)
+	{	putchar(a);
+		a++;
+	}
+	while (b < 103)
+	{       putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
