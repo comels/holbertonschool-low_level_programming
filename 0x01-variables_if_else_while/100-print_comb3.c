ccdf7a446ce44fb1adfa3dff3cb8e1da8d636b67
comels <come.l.s@hotmail.fr>
2022-02-07 16:11:41 +0000

    100print2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,30 +6,30 @@
 */
 int main(void)
 {
-    int a = 48;
-    int b = 49;
+int a = 48;
+int b = 49;
 
-    while (a < 57)
-    {
-    putchar(a);
-    putchar(b);
+while (a < 57)
+{
+putchar(a);
+putchar(b);
 
-    if (a < 56)
-    {
-    putchar(',');
-    putchar(' ');
-    }
+if (a < 56)
+{
+putchar(',');
+putchar(' ');
+}
 
-    if (b == 57)
-    {
-    a++;
-    b = a + 1;
-    }
-    else
-    {
-    b++;
-    }
-    }
-    putchar('\n');
-    return (0);
+if (b == 57)
+{
+a++;
+b = a + 1;
+}
+else
+{
+b++;
+}
+}
+putchar('\n');
+return (0);
 }
