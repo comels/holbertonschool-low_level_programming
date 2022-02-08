82d871648acc3ee693cfdd299ee51ce5c2a67bc1
comels <come.l.s@hotmail.fr>
2022-02-08 13:08:57 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - fonction
+ * @int: parametre
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	_putchar('\n');
+}
