fca5a8158cfff79c939ecfe0ce8db0a9df025870
comels <come.l.s@hotmail.fr>
2022-02-08 15:54:47 +0000

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,6 +7,18 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + '0');
+		return (-l);
+	}
+	else
+	{
+		-putchar(l + '0');
+		return (l);
+	}
 	_putchar('\n');
 }
