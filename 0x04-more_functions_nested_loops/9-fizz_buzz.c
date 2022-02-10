7ac4c331af37a427ddc4e55008c73ec43a374e59
comels <come.l.s@hotmail.fr>
2022-02-10 15:37:47 +0000

    Buzz2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,10 +11,10 @@ int main(void)
 	{
 		if (n == 100)
 			printf("Buzz");
-		else if (n % 3 == 0)
-			printf("Fizz ");
 		else if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
 		else if (n % 5 == 0)
 			printf("Buzz ");
 		else
