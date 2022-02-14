1f788e4548957e49b8d3c79e1596a2257e8c3897
comels <come.l.s@hotmail.fr>
2022-02-14 18:13:23 +0000

    copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i] >= '\0')
 	{
 		dest[i] = src[i];
-		if (src[i] == 0)
-			return(dest);
 		i++;
 	}
 	return (dest);
