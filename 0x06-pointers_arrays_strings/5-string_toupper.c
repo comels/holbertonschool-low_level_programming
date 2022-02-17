a4da00a5092fa4d22fca7df355735b9d669eb345
comels <come.l.s@hotmail.fr>
2022-02-17 08:48:53 +0000

    j1

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - fonction
+ * @s: param
  * Return: resultat
  */
 char *string_toupper(char *s)
@@ -9,7 +10,7 @@ char *string_toupper(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <=122)
+		if (s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
 	}
 	return (s);
