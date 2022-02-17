fd078c26112415c8f7aa10a9841a7bdac810e637
comels <come.l.s@hotmail.fr>
2022-02-17 10:52:20 +0000

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - fonction
+ * @s: param
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 58 && s[i] > 47)
+			s[i] = s[i];
+
+		else if (s[i] < 94 && s[i] > 64)
+			s[i] = s[i];
+
+		else if (s[i] < 123 && s[i] > 96)
+			s[i] = s[i];
+
+		else
+			if (s[i + 1] != '\0' && s[i + 1] < 123 && s[i + 1] > 96)
+				s[i + 1] -= 32;
+	}
+	return (s);
+}
