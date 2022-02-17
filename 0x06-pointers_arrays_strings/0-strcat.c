36ee2f462e788c487ebf70e865643b6bd7376ad3
comels <come.l.s@hotmail.fr>
2022-02-17 08:40:16 +0000

    j1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,11 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for(i = 0; dest[i] != '\0'; i++);
-	while(src[j] != '\0')
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
