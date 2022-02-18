8959879713de4b01d40d522329357343a7ef2f16
comels <come.l.s@hotmail.fr>
2022-02-18 15:41:49 +0000

    printnumber

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - fonction
+ * @n: param
+ * Return: rien
+ */
+void print_number(int n)
+{
+	int div = 1;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		n = n * -1;
+	}
+
+	while (n / (div * 10) != 0)
+	{
+		div = div * 10;
+	}
+
+	while (div > 0)
+	{
+		_putchar((n / div) + 48);
+		n = n % div;
+		div = div / 10;
+	}
+}
