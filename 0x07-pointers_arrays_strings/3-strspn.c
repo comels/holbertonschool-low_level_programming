dd76d2a4bede83412f1160d775d376435b2bbe9d
comels <come.l.s@hotmail.fr>
2022-02-21 14:32:29 +0000

    spn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * int _strspn - fonction
+ * @s: param
+ * @accept: param
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (k != i)
+			break;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				k++;
+		}
+	}
+	return (k);
+}
