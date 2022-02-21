cd1ce36f76f31f4a5da2326d21e5fe17d63f5fbc
comels <come.l.s@hotmail.fr>
2022-02-21 12:25:28 +0000

    chr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * @s: param
+ * @c: param
+ * Return: return
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
