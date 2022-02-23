ed6bde766861a09639dcec97435f27c174701e5f
comels <come.l.s@hotmail.fr>
2022-02-23 16:16:10 +0000

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * sqrt - fonction
+ * @n: param
+ * @x: param
+ * Return: int
+ */
+int sqrt(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+
+	if (x * x > n)
+		return (-1);
+
+	return (sqrt(n, x + 1));
+
+
+/**
+ * _sqrt_recursion - fonction
+ * @n: param
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt(n, 1));
+}
