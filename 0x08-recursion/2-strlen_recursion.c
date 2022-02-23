04d344149ec4dd0ae64cf9cf97adc03d164de43e
comels <come.l.s@hotmail.fr>
2022-02-23 11:30:29 +0000

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - fonction
+ * @s: param
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (_strlen_recursion(s + 1) + 1);
+	else
+		return (0);
+}
