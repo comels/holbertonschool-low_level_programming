d384b1a343fd6e8ab6ed10b91ee71933927be719
comels <come.l.s@hotmail.fr>
2022-02-24 12:09:36 +0000

    strch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,13 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
-	if (c == '\0' || s == 0)
-		return (0);
-
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
+	if (s[i] == c)
+		return (s + i);
 	return (0);
 }
