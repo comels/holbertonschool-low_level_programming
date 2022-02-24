b5169beda0ae78e9ca5be33c0764329397af6cf4
comels <come.l.s@hotmail.fr>
2022-02-24 10:57:36 +0000

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _sqrt - fonction
+ * @n: param
+ * @x: param
+ * Return: int
+ */
+int prime(int n, int x)
+{
+	if (x == n)
+		return (1);
+	if (n % x == 0)
+		return (0);
+
+	return (prime(n, x + 1));
+}
+
+/**
+ * is_prime_number - fonction
+ * @n: param
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+
+	return (prime(n, 2));
+}
