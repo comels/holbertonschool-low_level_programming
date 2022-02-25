8981b3e10c0436c725e34014817cd224de1a7ad0
comels <come.l.s@hotmail.fr>
2022-02-25 11:53:09 +0000

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	for (; i < argc; i++)
-		printf("%s\n", argv[i]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - fonction
+ * @argv: param
+ * @argc: param
+ * Return: 0
+ */
+int main(int argc, int *argv[])
+{
+	int i = 0;
+
+	for(; i < argc; i++)
