1daf278607175a17d1ac936247efe5384af39c9d
comels <come.l.s@hotmail.fr>
2022-02-25 11:06:07 +0000

    read

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int mul = 1;
 
-	if(argc < 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	for(; i < argc; i++)
+	for (; i < argc; i++)
 		mul *= atoi(argv[i]);
 	printf("%d\n", mul);
 	return (0);
