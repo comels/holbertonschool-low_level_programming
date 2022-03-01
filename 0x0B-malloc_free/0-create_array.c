6bc3d7de9e2e4c128adc7724ca75bbc9e0ff4e28
comels <come.l.s@hotmail.fr>
2022-03-01 08:58:42 +0000

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *create_array - fonction
+ * @size: param
+ * @c: param
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *str;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
