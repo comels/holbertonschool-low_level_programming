642b6244b251b8f0dabaed0d57b52a63179cbe72
comels <come.l.s@hotmail.fr>
2022-03-01 09:04:19 +0000

    array2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,9 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
+	if (str == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
