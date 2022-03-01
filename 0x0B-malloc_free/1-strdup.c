04b6fff8a86f23a5acb58049685e015b83270ce2
comels <come.l.s@hotmail.fr>
2022-03-01 10:01:17 +0000

    copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - fonction
+ * @str: param
+ * Return: cp
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *cp;
+
+	if (str == 0)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	cp = malloc(sizeof(char) * i);
+
+	if (cp == 0)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		cp[j] = str[j];
+
+	return (cp);
+}
