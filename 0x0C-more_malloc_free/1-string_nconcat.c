7a649f544de82662defa64bfb1050e8c3b88a0b3
comels <come.l.s@hotmail.fr>
2022-03-03 11:04:58 +0000

    con

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int size;
 	char *s3;
 
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
