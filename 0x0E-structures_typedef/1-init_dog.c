fa55046bc94d262ca4366a2799f15a8ed34335f4
comels <come.l.s@hotmail.fr>
2022-03-07 11:07:47 +0000

    read

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - fonction
+ * @d: Dog
+ * @name: Name dog
+ * @age: Age dog
+ * @owner: Owner dog
+ * Return: rien
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
