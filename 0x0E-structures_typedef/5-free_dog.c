1c4c28e63091c8bbfde84acc8b4cc21692743c15
comels <come.l.s@hotmail.fr>
2022-03-07 13:30:24 +0000

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog - fonction
+ * @d: Dog
+ * Return: rien
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
