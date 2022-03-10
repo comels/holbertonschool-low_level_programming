5bd31084bf6a2b484e5778551e74d7d198c03789
comels <come.l.s@hotmail.fr>
2022-03-10 14:56:05 +0000

    3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
 #ifndef _VARIADIC_
 #define _VARIADIC_
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+typedef struct print{
+	char *c;
+	void (*f)(va_list args);
+} print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
