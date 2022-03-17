e5d9843253d2a47444b12eecce1200cd6dbd032b
comels <come.l.s@hotmail.fr>
2022-03-17 12:28:11 +0000

    3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * list_len - fonction
+ * @h: param
+ * Return: i
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+	list_t *new;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (0);
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
