0d4a32e025d7abce3dc316194f94564c70858751
comels <come.l.s@hotmail.fr>
2022-03-22 09:44:40 +0000

    9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (count->next == NULL)
 			return (-1);
-	        count = count->next;
+		count = count->next;
 	}
 	tmp = count->next;
 	count->next = tmp->next;
