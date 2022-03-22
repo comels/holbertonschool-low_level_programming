44fe43ff30cde61300179eea03db93e2c593c220
comels <come.l.s@hotmail.fr>
2022-03-22 08:55:50 +0000

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
-
 	for (i = 0; i < index; i++)
 	{
 		if (head == NULL)
