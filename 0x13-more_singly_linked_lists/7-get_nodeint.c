60a7d2cba11348b887aea4ac486819c0f0958eb1
comels <come.l.s@hotmail.fr>
2022-03-22 08:34:15 +0000

    5

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index; i++)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
 	return (head);
