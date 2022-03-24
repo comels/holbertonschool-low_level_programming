362e4661393cbb57ac40035f77f27a126a8e2ef0
comels <come.l.s@hotmail.fr>
2022-03-24 14:56:42 +0000

    4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!n)
+	if (!n || index > 64)
 		return (-1);
 
-	 *n |= (1 << index);
-	 return (1);
+	*n |= (1 << index);
+	return (1);
 }
