4d532e045fb9b3c8c471068d0335decb815bb715
comels <come.l.s@hotmail.fr>
2022-03-28 12:35:08 +0000

    6

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* main - fonction
+* @argc: para
+* @argv: para
+* Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	int openfrom, opento, r, w;
+	char str[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	openfrom = open(argv[1], O_RDONLY);
+
+	if (openfrom == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read fromfile %s\n", argv[1]), exit(98);
+
+	opento = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (opento == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	
+	while ((r = read(openfrom, str, 1024)) > 0)
+	{
+		w = write(opento, str, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	
+
+	close(openfrom);
+	if (close(openfrom) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", openfrom), exit(100);
+
+	close(opento);
+	if (close(opento) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opento), exit(100);
+
+	return (0);
+}
