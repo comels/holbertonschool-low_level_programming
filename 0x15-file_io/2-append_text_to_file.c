92c1d12826271165117199bf2b2786dc39cb8b2b
comels <come.l.s@hotmail.fr>
2022-03-28 10:59:47 +0000

    5

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 		write(o, text_content, strlen(text_content));
-	
+
 	close(o);
 	return (1);
 }
