90d272607ba72480bd5d25633a33cdcf9ac3e1fd
comels <come.l.s@hotmail.fr>
2022-03-28 12:44:52 +0000

    6

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,8 +17,7 @@ int main(int argc, char **argv)
 
 	openfrom = open(argv[1], O_RDONLY);
 	if (openfrom == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from
-		file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	opento = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (opento == -1)
@@ -31,8 +30,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (r == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from
-		file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	close(openfrom);
 	if (close(openfrom) == -1)
