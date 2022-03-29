49c0ee649357162361ba660ef6dd98b6cb9135fb
comels <come.l.s@hotmail.fr>
2022-03-29 10:12:12 +0000

    7

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,23 +9,27 @@
 
 int main(int argc, char **argv)
 {
-	int openfrom, opento, r, w;
+	int openfrom, opento, r, w, closeto, closefrom;
 	char str[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	if ((openfrom = open(argv[1], O_RDONLY)) == -1)
+	openfrom = open(argv[1], O_RDONLY);
+	if (openfrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if ((opento = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664)) == -1)
+
+	opento = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (opento == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((r = read(openfrom, str, 1024)) > 0)
 	{
-		if ((w = write(opento, str, r)) == -1)
+		w = write(opento, str, r);
+		if (w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (r == -1)
@@ -33,10 +37,14 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if ((closefrom = close(openfrom)) == -1)
+
+	closefrom = close(openfrom);
+	if (closefrom == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", openfrom), exit(100);
 
-	if ((closeto = close(opento)) == -1)
+	closeto = close(opento);
+	if (closeto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opento), exit(100);
+
 	return (0);
 }
