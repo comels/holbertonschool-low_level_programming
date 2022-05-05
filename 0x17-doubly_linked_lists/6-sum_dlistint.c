8426d35106710d8e4ba96bb854b3c6007301a009
comels <come.l.s@hotmail.fr>
2022-05-05 11:21:05 +0000

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_dlistint - fonction that return a sum
+ * @head: List
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (tmp)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
